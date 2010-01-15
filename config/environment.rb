679e03600ee0befe5a69c39cb5be27e752f0874a
webmatze <webmatze@eventicus.com>
2010-01-15 22:26:50 +0100

    Deutsche Überzetzung hinzugefügt

--- config/environment.rb
+++ config/environment.rb
@@ -62,6 +62,7 @@ Rails::Initializer.run do |config|
   config.time_zone = "Berlin"
   
   #set locale
+  config.i18n.load_path << Dir[File.join(RAILS_ROOT, 'config', 'locales', '*.{rb,yml}')]
   config.i18n.default_locale = :en
   
 end
