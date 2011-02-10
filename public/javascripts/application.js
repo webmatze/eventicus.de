167938805f86fd9124299c6941e9cbda7049abd0
Mathias Karstädt <mathias.karstaedt@gmail.com>
2011-02-10 19:57:28 +0100

    Added script to find the users current location and put it into the where search field.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,24 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+Event.observe(window, 'load', function() {
+	//find the current users location and show it in the Where form field.
+	if ($('where')) {
+		var client = new simplegeo.ContextClient('kGfq4LhbLtxj7d3Cmph3QHZyngUK7NGn');
+		client.getLocation({enableHighAccuracy: true}, function(err, position) {
+		    if (err) {
+		        console.log(err);
+		    } else {
+						console.log(position);
+						client.getContext(position.coords.latitude, position.coords.longitude, function(err, data) {
+						    if (err) {
+						        console.error(err);
+						    } else {
+						        console.log(data);
+										$('where').value = data.features[0].name;
+						    }
+						});
+		    }
+		});
+	}
+})
