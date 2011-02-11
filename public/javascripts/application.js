1e8cea47a3f3b025e891b980f6f7c017e2d5dc4f
Mathias Karstädt <mathias.karstaedt@gmail.com>
2011-02-11 22:55:47 +0100

    safe geodata to sessionStorage if available, to minimize calls to simplegeo webservice

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -4,21 +4,33 @@
 Event.observe(window, 'load', function() {
 	//find the current users location and show it in the Where form field.
 	if ($('where')) {
-		var client = new simplegeo.ContextClient('kGfq4LhbLtxj7d3Cmph3QHZyngUK7NGn');
-		client.getLocation({enableHighAccuracy: true}, function(err, position) {
-		    if (err) {
-		        console.log(err);
-		    } else {
-						console.log(position);
-						client.getContext(position.coords.latitude, position.coords.longitude, function(err, data) {
-						    if (err) {
-						        console.error(err);
-						    } else {
-						        console.log(data);
-										$('where').value = data.features[0].name;
-						    }
-						});
-		    }
-		});
+		if (sessionStorage && sessionStorage.geoData) {
+			console.log("load geodata from sessionStorage");
+			console.log(sessionStorage.geoData);
+			$('where').value = sessionStorage.geoData;
+		}
+		else
+		{
+			var client = new simplegeo.ContextClient('kGfq4LhbLtxj7d3Cmph3QHZyngUK7NGn');
+			client.getLocation({enableHighAccuracy: true}, function(err, position) {
+			    if (err) {
+			        console.log(err);
+			    } else {
+							console.log(position);
+							client.getContext(position.coords.latitude, position.coords.longitude, function(err, data) {
+							    if (err) {
+							        console.error(err);
+							    } else {
+							        console.log(data);
+											if (sessionStorage && !sessionStorage.geoData) {
+												console.log("safe geodata into sessionStorage");
+												sessionStorage.geoData = data.features[0].name;
+											}
+											$('where').value = data.features[0].name;
+							    }
+							});
+			    }
+			});
+		}
 	}
 })
