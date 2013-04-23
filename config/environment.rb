5a230256d38943ad955bb93ec732a512eebb0471
webmatze <webmatze@eventicus.com>
2013-04-23 20:41:45 +0200

    added default encoding

--- config/environment.rb
+++ config/environment.rb
@@ -4,6 +4,8 @@
 # you don't control web/app server and can't set it the proper way
 # ENV['RAILS_ENV'] ||= 'production'
 
+Encoding.default_internal = 'UTF-8'
+
 # Specifies gem version of Rails to use when vendor/rails is not present
 RAILS_GEM_VERSION = '2.3.17' unless defined? RAILS_GEM_VERSION
 
